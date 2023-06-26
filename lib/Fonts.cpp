595dc3f5c48b4bbcf7137964e6feaeb79486816e
Destin Moulton <destin.moulton@gmail.com>
2023-06-26 15:59:14 -0600

    Added Fonts class

---
+++ lib/Fonts.cpp
@@ -0,0 +1,5 @@
+//
+// Created by destin on 6/26/23.
+//
+
+#include "Fonts.h"
---
+++ lib/Fonts.h
@@ -0,0 +1,14 @@
+//
+// Created by destin on 6/26/23.
+//
+
+#ifndef SQUEAL_FONTS_H
+#define SQUEAL_FONTS_H
+
+
+class Fonts {
+
+};
+
+
+#endif //SQUEAL_FONTS_H
