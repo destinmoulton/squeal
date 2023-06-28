7258c5428a368a4583448b6387f4ff7d4315add6
Destin Moulton <destin.moulton@gmail.com>
2023-06-28 09:52:31 -0600

    Added basic gui class

---
+++ lib/gui.h
@@ -0,0 +1,20 @@
+//
+// Created by destin on 6/27/23.
+//
+
+#ifndef SQUEAL_GUI_H
+#define SQUEAL_GUI_H
+
+
+#include <GLFW/glfw3.h> // system OpenGL headers
+
+class GUI {
+
+public:
+    static int run();
+
+    //static GLFWerrorfun glfw_error_callback(int error, const char *description);
+};
+
+
+#endif //SQUEAL_GUI_H
